8af30815b0e765a2566fe67e025dfa69bb7657c6
Nerower <nerower.k@gmail.com>
2024-10-07 16:42:23 +0300

    l0: Найти точки пересечения квадратичных функций

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -30,8 +30,61 @@ def functions(a_1, a_2):
     Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
-    # put your code here
-    pass
+    # Извлечь коэф-ты ур-ний
+    factors1 = np.array([int(c) for c in a_1.split(' ')])
+    factors2 = np.array([int(c) for c in a_2.split(' ')])
+
+    # Найти экстремумы
+    extrem1 = extrem(*factors1)
+    extrem2 = extrem(*factors2)
+    
+    # Найти точки пересечения
+    factors = factors1 - factors2
+    roots = solve_equation(*factors)
+    if roots == None:
+        return None
+
+    func = to_polinomial(factors1)
+    return [(x, func(x)) for x in roots]
+
+def to_polinomial(factors):
+    n = len(factors)
+    return lambda x: sum(x**i * factors[::-1][i] for i in range(n))
+
+def extrem(a, b, c):
+    if a == 0:
+        return None
+    func = to_polinomial([a, b, c])
+    x = -b / (2*a)
+    return (x, func(x))
+
+def solve_equation(a, b, c):
+    if (a == 0):
+        if (b == 0):
+            # Корней бесконечно много или нет 
+            return None if c == 0 else []
+        else:
+            # Линейное ур-ние
+            return [-c / b]
+
+    # Дискриминант
+    discr = b**2 - 4*a*c
+
+    print(f"{a}x^2 + {b}x + {c}")
+    print(f"Discr = {discr}")
+
+    if (discr < 0):
+        # Корней нет
+        return []
+    elif (discr == 0):
+        # Корни равны
+        x = -b / (2*a)
+        return [x]
+    else:
+        # Разные корни
+        x1 = (-b + discr**(0.5)) / (2*a)
+        x2 = (-b - discr**(0.5)) / (2*a)
+        return [x1, x2]
 
 
 def skew(x):
